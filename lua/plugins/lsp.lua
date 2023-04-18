1366583db6ce28cbfbbd859ba7ffaea0f7b6cc8d
Andres Vargas <devandresvargas@gmail.com>
2023-04-18 18:44:51 -0500

    start-up improvement

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,19 +7,8 @@ return {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
         "WhoIsSethDaniel/mason-tool-installer.nvim",
+        "lvimuser/lsp-inlayhints.nvim"
 
-
-        -- Autocompletion
-        'hrsh7th/nvim-cmp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'saadparwaiz1/cmp_luasnip',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-nvim-lua',
-
-        -- Snippets
-        'L3MON4D3/LuaSnip',
-        'rafamadriz/friendly-snippets',
     },
     config = function()
         require "alpha.lsp"
